48258a176e95ff5e652435a582b99acc5ecac417
Shivansh Vikram Singh <shivanshvikramsingh764@gmail.com>
2024-12-26 23:06:40 +0530

    modified backend for auth

--- AiDiaryBackend/database/models/User.js
+++ AiDiaryBackend/database/models/User.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password_hash: { type: String, required: true },
-    created_at: { type: Date, default: Date.now }
-  });
-  const User = mongoose.model('User', userSchema);
-  module.exports = User;
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', UserSchema);
